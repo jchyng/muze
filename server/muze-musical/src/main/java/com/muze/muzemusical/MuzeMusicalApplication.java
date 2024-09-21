ea30bc1f11b4067b001a0755100f62dafc22b662
jchyng <jcy0342@gmail.com>
2024-09-22 00:30:27 +0900

    [init] server 프로젝트 초기 세팅

---
+++ server/muze-musical/src/main/java/com/muze/muzemusical/MuzeMusicalApplication.java
@@ -0,0 +1,13 @@
+package com.muze.muzemusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzeMusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzeMusicalApplication.class, args);
+	}
+
+}
---
+++ server/muze-musical/src/test/java/com/muze/muzemusical/MuzeMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muze.muzemusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzeMusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
